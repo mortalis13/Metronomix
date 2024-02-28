f1951dadcf651b5f06455274199561ad2e37e620
mortalis13 <stangerroman@gmail.com>
2024-02-28 22:42:06 +0100

    output to docs folder without browser subfolder

--- docs-src/src/app/app.component.ts
+++ docs-src/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-import { MainPageComponent } from './main-page/main-page.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
